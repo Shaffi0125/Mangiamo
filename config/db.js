606426138e7ff04d32aa5dbf65085009fb18ec0a
Shaffi <104960954+Shaffi0125@users.noreply.github.com>
2025-02-04 12:58:41 +0530

    Delete config directory

--- config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://shaffi:HouSeGHAr@cluster0.2zzsd.mongodb.net/food-del').then(() => console.log("DB Connected"));
-}
